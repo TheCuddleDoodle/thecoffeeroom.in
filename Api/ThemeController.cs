3bc063963b93ffb4e5b514dd47562a3b216401b4
jasmeet singh <jskainthofficial@gmail.com>
2023-07-21 11:31:16 +0530

    👽️ Api update

---
+++ Api/ThemeController.cs
@@ -0,0 +1,43 @@
+﻿using Coffeeroom.Core.Helpers;
+using Coffeeroom.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Coffeeroom.Api
+{
+    [Route("api/themer")]
+    [ApiController]
+    public class ThemeController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            List<Theme> themes = new();
+            string connectionString = ConfigHelper.NewConnectionString;
+            using (SqlConnection connection = new(connectionString))
+            {
+                connection.Open();
+                string sql;
+                sql = "SELECT * FROM TblThemeMaster WHERE Id=@Id";
+                using SqlCommand command = new(sql, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                using SqlDataReader dataReader = await command.ExecuteReaderAsync();
+
+                while (await dataReader.ReadAsync())
+                {
+                    Theme entry = new()
+                    {
+                        Title = (string)dataReader["Title"],
+                        Description = (string)dataReader["Description"],
+                        String = (string)dataReader["String"],
+                        FontLink = (string)dataReader["FontLink"],
+                    };
+                    themes.Add(entry);
+                }
+            }
+            return new JsonResult(themes);
+        }
+
+    }
+}
