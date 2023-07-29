09513c6f1c4ef90b06ecfd9cf0beda82b112109e
jasmeet singh <jskainthofficial@gmail.com>
2023-07-30 00:28:12 +0530

    🗃️ tsql migrations

---
+++ Migrations/TblPasswordReset.sql
@@ -0,0 +1,19 @@
+﻿USE [coffeeroomdb]
+GO
+
+/****** Object:  Table [jsm33t].[TblPasswordReset]    Script Date: 7/29/2023 2:51:36 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TblPasswordReset](
+	[Id] [int] NOT NULL,
+	[UserId] [int] NOT NULL,
+	[Token] [nvarchar](50) NOT NULL,
+	[DateAdded] [datetime] NOT NULL,
+	[IsValid] [bit] NULL
+) ON [PRIMARY]
+GO
+
