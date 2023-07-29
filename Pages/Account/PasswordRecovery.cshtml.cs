ca3c7f0ce7d746b9973b8ee7c1f442368a43d193
jasmeet singh <jskainthofficial@gmail.com>
2023-07-30 00:29:06 +0530

    📄 web pages update

--- Pages/Account/PasswordRecovery.cshtml.cs
+++ Pages/Account/PasswordRecovery.cshtml.cs
@@ -1,3 +1,7 @@
+using Coffeeroom.Core.Helpers;
+using Coffeeroom.Models.Domain;
+using Coffeeroom.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,27 @@ namespace Coffeeroom.Pages.Account
 {
     public class PasswordRecoveryModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGetAsync()
         {
+            var sessionStat = HttpContext.Session.GetString("role");
+            if (sessionStat != null)
+            {
+                if (sessionStat.ToString() == "guest")
+                {
+                    return LocalRedirect("/account/login");
+                    
+                }
+                else
+                {
+                    return Page();
+                }
+                
+            }
+            else
+            {
+                return LocalRedirect("/account/login");
+            }
         }
+
     }
 }
---
+++ Pages/Account/SetPassword.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coffeeroom.Pages.Account
+{
+    public class SetPasswordModel : PageModel
+    {
+        public void OnGet(string userid,string token)
+        {
+            
+        }
+    }
+}
