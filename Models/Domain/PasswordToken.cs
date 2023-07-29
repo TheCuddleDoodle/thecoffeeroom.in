4f4d92fca0ed6353c724f625d06385e225e0e912
jasmeet singh <jskainthofficial@gmail.com>
2023-07-30 00:28:52 +0530

    📦️ models

---
+++ Models/Domain/PasswordToken.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coffeeroom.Models.Domain
+{
+    public class PasswordToken
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public DateTime DateAdded{ get; set; }
+
+        [Required]
+        public bool IsValid { get; set; }
+    }
+}
---
+++ Models/View/SignUpVals.cs
@@ -0,0 +1,12 @@
+﻿namespace Coffeeroom.Models.View
+{
+    public class SignUpVals
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EMail { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string Otp { get; set; }
+    }
+}
