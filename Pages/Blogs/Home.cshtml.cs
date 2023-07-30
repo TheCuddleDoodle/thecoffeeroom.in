6888404bb9e68d03d5f7b9d60f43d9c941c27af8
jasmeet singh <jskainthofficial@gmail.com>
2023-07-30 10:06:53 +0530

    vue integration forblog and account

---
+++ Pages/Blogs/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coffeeroom.Pages.Blogs
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
